ffb2a286d5b2d4067166a5f7b8c2c3f9685822f6
Andy D. Ng <10527554+andyngdz@users.noreply.github.com>
2022-04-18 23:11:04 +0700

    chore: added download status

--- lib/widgets/youtube_download_button.dart
+++ lib/widgets/youtube_download_button.dart
@@ -4,13 +4,13 @@ import 'package:block_downloader/colors.dart';
 import 'package:block_downloader/services/file.dart';
 import 'package:block_downloader/services/youtube.dart';
 import 'package:block_downloader/theme.dart';
+import 'package:block_downloader/types/download.dart';
 import 'package:block_downloader/widgets/youtube_quality_selector.dart';
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:popover/popover.dart';
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
-class YoutubeDownloadButton extends ConsumerStatefulWidget {
+class YoutubeDownloadButton extends StatefulWidget {
   final Video video;
   final StreamManifest streamManifest;
 
@@ -24,7 +24,11 @@ class YoutubeDownloadButton extends ConsumerStatefulWidget {
   YoutubeDownloadButtonState createState() => YoutubeDownloadButtonState();
 }
 
-class YoutubeDownloadButtonState extends ConsumerState<YoutubeDownloadButton> {
+class YoutubeDownloadButtonState extends State<YoutubeDownloadButton> {
+  DownloadStatus downloadStatus = DownloadStatus.idle;
+  num percent = 0;
+  num downloadedBytes = 0;
+
   void onSelected(StreamInfo streamInfo) async {
     if (await fileService.isDownloadFolderExists) {
       Stream stream = yt.videos.streamsClient.get(streamInfo);
@@ -34,8 +38,34 @@ class YoutubeDownloadButtonState extends ConsumerState<YoutubeDownloadButton> {
       File file = File(tempSavePath);
       IOSink sink = file.openWrite();
 
-      stream.pipe(sink);
+      stream.listen(
+        (bytes) {
+          onAddedData(bytes, sink, streamInfo);
+        },
+        onError: (error) {},
+        onDone: () async {
+          await sink.flush();
+          await sink.close();
+
+          setState(() {
+            downloadedBytes = 0;
+            percent = 0;
+            downloadStatus = DownloadStatus.success;
+          });
+        },
+        cancelOnError: true,
+      );
     }
+  }
+
+  void onAddedData(List<int> bytes, IOSink sink, StreamInfo streamInfo) {
+    sink.add(bytes);
+
+    setState(() {
+      downloadedBytes += bytes.length;
+      percent = (downloadedBytes / streamInfo.size.totalBytes * 100).floor();
+      downloadStatus = DownloadStatus.downloading;
+    });
   }
 
   void onDownloaded() {
